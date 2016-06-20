98f4ee91c547f966f159ffec2d2938b5e4cc03e7
Patrick Mézard <patrick@clustree.com>
2016-06-20 09:52:50 +0200

    setup.py: this is 2016, enable C++11

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ noaho_module = Extension(
       'src/array-aho.cpp'],
     depends=[
         'src/array-aho.h',
-        'src/noahong.pyx']
+        'src/noahong.pyx'],
+    extra_compile_args=["-std=c++11"],
+    extra_link_args=["-std=c++11"],
 )
 
 version = '0.9.7'
